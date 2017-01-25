a63348d43a220d99845882f33bd09e6753a8dddd
Joshua Stewart <josh211505857@gmail.com>
2017-01-25 12:43:46 +0200

    Created Tyring to deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyDUq7iXyyK_oXSVyeftb5hj5rplz8o3a3M",
+    authDomain: "contacts-ad20d.firebaseapp.com",
+    databaseURL: "https://contacts-ad20d.firebaseio.com",
+    storageBucket: "contacts-ad20d.appspot.com",
+    messagingSenderId: "414241529872"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
